3ca82feca024d3f76bdd02c6450a69e3bf5f0769
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2023-10-17 06:50:31 +0100

    chore: add empty state icon

--- src/components/dashboard/clickup/content-section.tsx
+++ src/components/dashboard/clickup/content-section.tsx
@@ -1,8 +1,10 @@
 import { CollapseIcon } from "@/components/icons";
 import {
+  Box,
   Container,
   Flex,
   Heading,
+  Icon,
   IconButton,
   Link,
   Stack,
@@ -128,6 +130,15 @@ export const ContentSection: React.FC<{
           </Stack>
         ) : (
           <VStack h="100%" justifyContent="center">
+            <VStack borderWidth="1px" rounded="50%" boxSize={8}>
+              <Icon viewBox="0 0 24 24">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                />
+              </Icon>
+            </VStack>
             <Heading textAlign="center" size="xs">
               Content Panel
             </Heading>
